8e315aab7cd4abf10459d1168c525ffcdba57564
luizvn <luizvinicius128@gmail.com>
2025-10-18 16:33:57 -0300

    build: add AuthModule

--- apps/notifications-api/src/app.module.ts
+++ apps/notifications-api/src/app.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    AuthModule,
+    NotificationsModule,
+    DatabaseModule,
+  ],
   controllers: [],
   providers: [],
 })
---
+++ apps/notifications-api/src/auth/auth.module.ts
@@ -0,0 +1,24 @@
+import {Module} from "@nestjs/common";
+import {PassportModule} from "@nestjs/passport";
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET', 'secret'),
+        signOptions: {
+          expiresIn: +configService.get<string>('JWT_EXPIRES_IN', '3600'),
+        },
+      }),
+    }),
+  ],
+    controllers: [AuthController],
+    providers: [AuthService, JwtStrategy,],
+    exports: [AuthService, JwtModule],
+})
+export class AuthModule {}
