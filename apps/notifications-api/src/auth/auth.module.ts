a51986f8cf70cabaaee7dcf1ca896f70b65bc833
luizvn <luizvinicius128@gmail.com>
2025-10-27 20:00:57 -0300

    feat: add AuthModule + JwtStrategy

--- apps/notifications-api/src/auth/auth.module.ts
+++ apps/notifications-api/src/auth/auth.module.ts
@@ -2,6 +2,9 @@ import {Module} from "@nestjs/common";
 import {PassportModule} from "@nestjs/passport";
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
@@ -18,7 +21,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     }),
   ],
     controllers: [AuthController],
-    providers: [AuthService, JwtStrategy,],
+    providers: [AuthService, JwtStrategy],
     exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
